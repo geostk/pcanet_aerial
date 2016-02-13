2d003ef279076cf7f3463677d44a220e0c152177
Marko Ivanovic <marko.ivanovic@yandex.com>
2016-02-13 14:03:44 +0100

    PCN hardcoded to use two layer because of problem with memory consumption

--- pcanet_aerial/PCANet_FeaExt.m
+++ pcanet_aerial/PCANet_FeaExt.m
@@ -43,64 +43,67 @@ end
 
 clear InImg;
 
-function [OutImg ImgIdx] = process_stage(stage, PCANet, InImg, InImgIdx, V)
-    prevNumFilters = PCANet.NumFilters(stage - 1);
-    InImgSubsets = cell(prevNumFilters, 1);
-    InImgIdxSubsets = cell(prevNumFilters, 1);
-    ImgPerSubset = length(InImg)/prevNumFilters;
-
-    % initialize subsets:
-    for i = 1:prevNumFilters
-      InImgSubsets{i} = cell(ImgPerSubset, 1);
-      InImgIdxSubsets{i} = zeros(ImgPerSubset, 1);
-      for j = 1:ImgPerSubset
-        InImgSubsets{i}{j} = zeros(size(InImg{1}));
-        InImgIdxSubsets{i}(j) = InImgIdx(j*prevNumFilters);
-      end
-
-    end
-
-    % put input images in right subsets:
-    for i = 1:length(InImg)
-      fnum = mod(i, prevNumFilters) + 1; % ith image is produced by fnumth filter from prevous layer
-      imgorig = ceil(i / prevNumFilters); % ith image is produced by convoluting imgorigth input image from previous layer
-
-      for j = 1:prevNumFilters
-        if PCANet.MappingMatrices{stage - 1}(fnum, j)
-          InImgSubsets{j}{imgorig} = InImgSubsets{j}{imgorig} + InImg{i};
-        end
-      end
-    end
-
-  OutImgSubsets = cell(prevNumFilters, 1);
-  OutImgIdxSubsets = cell(prevNumFilters, 1);
-
-  % compute filters' outputs:
-  for i = 1:prevNumFilters
-    [OutImgSubsets{i} OutImgIdxSubsets{i}] = PCA_output(InImgSubsets{i}, InImgIdxSubsets{i}, ...
-         PCANet.PatchSize(stage), PCANet.NumFilters(stage), V.filters{i}, PCANet.PoolingPatchSize);
-  end
-
-  % propagate to the next layer if required:
-  if stage < PCANet.NumStages
-    for i = 1:prevNumFilters
-      [OutImgSubsets{i} OutImgIdxSubsets{i}] = process_stage(stage + 1, PCANet, OutImgSubsets{i}, OutImgIdxSubsets{i}, V.next_stage{i});
-    end
-  end
-
-  % assemble output:
-  OutImg = vertcat(OutImgSubsets{:});
-  ImgIdx = vertcat(OutImgIdxSubsets{:});
-
-end %end of process_stage
-
 [OutImg ImgIdx] = PCA_output(OutImg, ImgIdx, ...
-     PCANet.PatchSize(1), PCANet.NumFilters(1), V.filters{1}, PCANet.PoolingPatchSize);
+     PCANet.PatchSize(1), PCANet.NumFilters(1), V.filters{1}, PCANet.PoolingPatchSize(1));
 
-if PCANet.NumStages > 1
-  [OutImg ImgIdx] = process_stage(2, PCANet, OutImg, ImgIdx, V.next_stage{1});
+stage = 2;
+InImg = OutImg;
+InImgIdx = ImgIdx;
+clear OutImg;
+clear ImgIdx;
+V = V.next_stage{1};
+
+
+prevNumFilters = PCANet.NumFilters(stage - 1);
+ImgSubsets = cell(prevNumFilters, 1);
+ImgIdxSubsets = cell(prevNumFilters, 1);
+ImgPerSubset = length(InImg)/prevNumFilters;
+
+% initialize subsets:
+for i = 1:prevNumFilters
+ ImgSubsets{i} = cell(ImgPerSubset, 1);
+ ImgIdxSubsets{i} = zeros(ImgPerSubset, 1);
+ for j = 1:ImgPerSubset
+     ImgSubsets{i}{j} = zeros(size(InImg{1}));
+     ImgIdxSubsets{i}(j) = InImgIdx(j*prevNumFilters);
+ end
+
 end
 
-[f BlkIdx] = HashingHist(PCANet,ImgIdx,OutImg);
+% put input images in right subsets:
+for i = 1:length(InImg)
+ fnum = mod(i, prevNumFilters) + 1; % ith image is produced by fnumth filter from prevous layer
+ imgorig = ceil(i / prevNumFilters); % ith image is produced by convoluting imgorigth input image from previous layer
+
+ for j = 1:prevNumFilters
+     if PCANet.MappingMatrices{stage - 1}(fnum, j)
+         ImgSubsets{j}{imgorig} = ImgSubsets{j}{imgorig} + InImg{i};
+     end
+ end
+end
+
+clear InImg;
+clear InImgIdx;
+
+ft = cell(prevNumFilters,1);
+
+% compute filters' outputs:
+for i = 1:prevNumFilters
+ [ImgSubsets{i} ImgIdxSubsets{i}] = PCA_output(ImgSubsets{i}, ImgIdxSubsets{i}, ...
+         PCANet.PatchSize(stage), PCANet.NumFilters(stage), V.filters{i}, PCANet.PoolingPatchSize(stage));
+
+ [ft{i}] = sparse(HashingHist(PCANet, ImgIdxSubsets{i}, ImgSubsets{i}));
+
+ ImgIdxSubsets{i} = [];
+ ImgSubsets{i} = [];
+end
+
+
+clear ImgSubsets;
+clear ImgIdxSubsets;
+
+f = sparse(vertcat(ft{:}));
+
+BlkIdx = [];
 
 end
--- pcanet_aerial/PCANet_train.m
+++ pcanet_aerial/PCANet_train.m
@@ -40,70 +40,12 @@ ImgIdx = (1:NumImg)';
 
 
 if exist('ImgFormat') & strcmp(ImgFormat,'color')
-  [OutImg ImgIdx] = separate_image_layers(InImg, ImgIdx);
+          [OutImg ImgIdx] = separate_image_layers(InImg, ImgIdx);
 else
-  OutImg = InImg;
+          OutImg = InImg;
 end
 
-function [OutImg ImgIdx V] = process_stage(stage, PCANet, InImg, InImgIdx)
-
-    prevNumFilters = PCANet.NumFilters(stage - 1);
-    InImgSubsets = cell(prevNumFilters, 1);
-    InImgIdxSubsets = cell(prevNumFilters, 1);
-    ImgPerSubset = length(InImg)/prevNumFilters;
-
-    % initialize subsets:
-    for i = 1:prevNumFilters
-      InImgSubsets{i} = cell(ImgPerSubset, 1);
-      InImgIdxSubsets{i} = zeros(ImgPerSubset, 1);
-      for j = 1:ImgPerSubset
-        InImgSubsets{i}{j} = zeros(size(InImg{1}));
-        InImgIdxSubsets{i}(j) = InImgIdx(j*prevNumFilters);
-      end
-
-    end
-
-    % put input images in right subsets:
-    for i = 1:length(InImg)
-      fnum = mod(i, prevNumFilters) + 1; % ith image is produced by fnumth filter from prevous layer
-      imgorig = ceil(i / prevNumFilters); % ith image is produced by convoluting imgorigth input image from previous layer
-
-      for j = 1:prevNumFilters
-        if PCANet.MappingMatrices{stage - 1}(fnum, j)
-          InImgSubsets{j}{imgorig} = InImgSubsets{j}{imgorig} + InImg{i};
-        end
-      end
-    end
-
-    % create filters:
-    V.filters = cell(prevNumFilters, 1);
-    for i = 1:prevNumFilters
-      V.filters{i} = PCA_FilterBank(InImgSubsets{i}, PCANet.PatchSize(stage), PCANet.NumFilters(stage));
-    end
-
-  OutImgSubsets = cell(prevNumFilters, 1);
-  OutImgIdxSubsets = cell(prevNumFilters, 1);
-
-  % compute filters' outputs:
-  for i = 1:prevNumFilters
-    [OutImgSubsets{i} OutImgIdxSubsets{i}] = PCA_output(InImgSubsets{i}, InImgIdxSubsets{i}, ...
-         PCANet.PatchSize(stage), PCANet.NumFilters(stage), V.filters{i}, PCANet.PoolingPatchSize);
-  end
-
-  % propagate to the next layer if required:
-  if stage < PCANet.NumStages
-    display(['Processing layer ' num2str(stage + 1) '...'])
-    V.next_stage = cell(prevNumFilters, 1);
-    for i = 1:prevNumFilters
-      [OutImgSubsets{i} OutImgIdxSubsets{i} V.next_stage{i}] = process_stage(stage + 1, PCANet, OutImgSubsets{i}, OutImgIdxSubsets{i});
-    end
-  end
-
-  % assemble output:
-  OutImg = vertcat(OutImgSubsets{:});
-  ImgIdx = vertcat(OutImgIdxSubsets{:});
-
-end % end of process_stage
+clearvars TrnData_ImgCell
 
 display(['Processing layer ' num2str(1) '...'])
 
@@ -112,13 +54,79 @@ V.next_stage = cell(1,1);
 V.filters{1} = PCA_FilterBank(OutImg, PCANet.PatchSize(1), PCANet.NumFilters(1));
 
 [OutImg ImgIdx] = PCA_output(OutImg, ImgIdx, ...
-     PCANet.PatchSize(1), PCANet.NumFilters(1), V.filters{1}, PCANet.PoolingPatchSize);
+     PCANet.PatchSize(1), PCANet.NumFilters(1), V.filters{1}, PCANet.PoolingPatchSize(1));
 
-if PCANet.NumStages > 1
-  display(['Processing layer ' num2str(2) '...'])
-  [OutImg ImgIdx V.next_stage{1}] = process_stage(2, PCANet, OutImg, ImgIdx);
+display(['Processing layer ' num2str(2) '...'])
+stage = 2;
+InImg = OutImg;
+InImgIdx = ImgIdx;
+clear OutImg;
+clear ImgIdx;
+
+
+prevNumFilters = PCANet.NumFilters(stage - 1);
+ImgSubsets = cell(prevNumFilters, 1);
+ImgIdxSubsets = cell(prevNumFilters, 1);
+ImgPerSubset = length(InImg)/prevNumFilters;
+
+% initialize subsets:
+for i = 1:prevNumFilters
+    ImgSubsets{i} = cell(ImgPerSubset, 1);
+    ImgIdxSubsets{i} = zeros(ImgPerSubset, 1);
+    for j = 1:ImgPerSubset
+        ImgSubsets{i}{j} = zeros(size(InImg{1}));
+        ImgIdxSubsets{i}(j) = InImgIdx(j*prevNumFilters);
+    end
+
 end
 
-[f BlkIdx] = HashingHist(PCANet,ImgIdx,OutImg);
+% put input images in right subsets:
+for i = 1:length(InImg)
+    fnum = mod(i, prevNumFilters) + 1; % ith image is produced by fnumth filter from prevous layer
+    imgorig = ceil(i / prevNumFilters); % ith image is produced by convoluting imgorigth input image from previous layer
+
+    for j = 1:prevNumFilters
+        if PCANet.MappingMatrices{stage - 1}(fnum, j)
+            ImgSubsets{j}{imgorig} = ImgSubsets{j}{imgorig} + InImg{i};
+        end
+    end
+end
+
+clear InImg;
+clear InImgIdx;
+
+display('cleared mem, shoud be OK now');
+
+% create filters:
+V2.filters = cell(prevNumFilters, 1);
+for i = 1:prevNumFilters
+    V2.filters{i} = PCA_FilterBank(ImgSubsets{i}, PCANet.PatchSize(stage), PCANet.NumFilters(stage));
+end
+V.next_stage{1} = V2;
+
+display('computing second layer outputs');
+
+ft = cell(prevNumFilters,1);
+
+
+% compute filters' outputs:
+for i = 1:prevNumFilters
+    [ImgSubsets{i} ImgIdxSubsets{i}] = PCA_output(ImgSubsets{i}, ImgIdxSubsets{i}, ...
+            PCANet.PatchSize(stage), PCANet.NumFilters(stage), V2.filters{i}, PCANet.PoolingPatchSize(stage));
+
+    [ft{i}] = sparse(HashingHist(PCANet, ImgIdxSubsets{i}, ImgSubsets{i}));
+
+    ImgIdxSubsets{i} = [];
+    ImgSubsets{i} = [];
+end
+
+
+clear ImgSubsets;
+clear ImgIdxSubsets;
+
+display('vert cat');
+f = sparse(vertcat(ft{:}));
+
+BlkIdx = [];
 
 end
